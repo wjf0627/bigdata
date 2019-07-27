59348332e89126265475ee4702acb56f9a7e0414
jinfeng.wang <jinfeng.wang@mobvista.com>
2019-07-27 10:28:30 +0800

    init clickhouse

---
+++ clickhouse/src/main/scala/com/jinfeng/clickhouse/CKExample.scala
@@ -0,0 +1,71 @@
+package com.jinfeng.clickhouse
+
+import java.util.{Random, UUID}
+
+import com.jinfeng.clickhouse.entity.Device
+import com.jinfeng.clickhouse.util.ClickHouseConnectionFactory
+import com.jinfeng.clickhouse.util.ClickHouseSparkExt._
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @package: com.jinfeng.clickhouse
+  * @author: wangjf
+  * @date: 2019/5/20
+  * @time: 下午3:16
+  * @email: wjf20110627@163.com
+  * @phone: 152-1062-7698
+  */
+class CKExample extends Serializable {
+
+  protected def run(args: Array[String]) {
+    val spark = SparkSession
+      .builder()
+      .appName("CKExample")
+      .master("local[10]")
+      .config("spark.rdd.compress", "true")
+      .config("spark.io.compression.codec", "lz4")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate()
+    try {
+
+      val sc = spark.sparkContext
+      sc.hadoopConfiguration.set("dfs.client.socket-timeout", "300000")
+      // clickhouse params
+      val host = "localhost"
+      val db = "wangjf"
+      val tableName = "device_set"
+      //  cluster configuration must be defined in config.xml (clickhouse config)
+      //  val clusterName = Some("localhost"): Option[String]
+      val clusterName = None
+
+      val random = new Random
+      import spark.implicits._
+      // define clickhouse datasource
+      implicit val clickhouseDataSource = ClickHouseConnectionFactory.get(host)
+      val df = sc.parallelize(1 to 10000000).map(f = _ => {
+        Device("" + UUID.randomUUID(), Array(
+          random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100),
+          random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100),
+          random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100),
+          random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100),
+          random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100)))
+      }).toDF()
+
+      df.createClickHouseDb(db, clusterName)
+      df.createClickHouseTable(db, tableName, Seq("dt"), Seq("device_id", "offer_id"), Seq(), clusterName)
+      df.saveToClickHouse(db, tableName, Seq("2019-07-15"), Seq("dt"), clusterName)
+
+    } finally {
+      if (spark != null) {
+        spark.stop()
+      }
+    }
+  }
+}
+
+
+object CKExample {
+  def main(args: Array[String]): Unit = {
+    new CKExample().run(args)
+  }
+}
---
+++ clickhouse/src/main/scala/com/jinfeng/clickhouse/entity/Device.scala
@@ -0,0 +1,11 @@
+package com.jinfeng.clickhouse.entity
+
+/**
+  * @package: com.jinfeng.clickhouse.entity
+  * @author: wangjf
+  * @date: 2019/5/24
+  * @time: 下午1:58
+  * @email: wjf20110627@163.com
+  * @phone: 152-1062-7698
+  */
+case class Device(device_id: String, offer_id: Array[Int]) extends Serializable
