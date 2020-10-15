e340d2f2f9a591df2f1b30e9b5709d3adec8efaa
jinfeng.wang <jinfeng.wang@mobvista.com>
2020-10-15 17:35:44 +0800

    init clickhouse

--- flink/src/main/java/com/jinfeng/flink/example/FlinkCassandraTuple.java
+++ flink/src/main/java/com/jinfeng/flink/example/FlinkCassandraTuple.java
@@ -1,8 +1,10 @@
 package com.jinfeng.flink.example;
 
-import com.datastax.driver.core.*;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.CodecRegistry;
+import com.datastax.driver.core.KeyspaceMetadata;
+import com.datastax.driver.core.TupleType;
 import com.datastax.driver.mapping.Mapper;
-import com.esotericsoftware.kryo.Serializer;
 import com.jinfeng.flink.entity.DevicePojo;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
@@ -36,19 +38,6 @@ public class FlinkCassandraTuple {
     public static void main(String[] args) throws Exception {
         String schema = "struct<device_id:string,age:string,gender:string,install:array<string>,interest:array<string>,frenquency:array<struct<tag:string,cnt:string>>>";
 
-        //  RowTypeInfo schema
-        //  Properties properties = new Properties();
-
-        // set up the batch execution environment
-        /*
-        properties.setProperty("cassandra.maxRequestsPerConnection", "80");
-        properties.setProperty("cassandra.maxConnectionsPerHost", "10");
-        properties.setProperty("cassandra.coreConnectionsPerHost", "1");
-        properties.setProperty("cassandra.port", "9042");
-        properties.setProperty("cassandra.ttl", "1468800");
-        properties.setProperty("cassandra.local.host", "127.0.0.1");
-         */
-
         ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
         Configuration configuration = new Configuration();
 
@@ -67,11 +56,9 @@ public class FlinkCassandraTuple {
                 return builder.addContactPoints("127.0.0.1").build();
             }
         };
-        //  tupleType = clusterBuilder.getCluster().getMetadata().newTupleType(DataType.text(), DataType.text());
         keyspaceMetadata = clusterBuilder.getCluster().getMetadata().getKeyspace("dmp_realtime_service");
 
         DataSet<DevicePojo> dataSet = orcTableSource.getDataSet(env).map(new ConvertPojo());
-        //  .filter(new DeviceFilter());
 
         dataSet.output(new CassandraPojoOutputFormat<>(clusterBuilder, DevicePojo.class, () -> new Mapper.Option[]{Mapper.Option.saveNullFields(false)}));
         env.execute("write");
@@ -80,14 +67,14 @@ public class FlinkCassandraTuple {
     private static final class ConvertPojo extends RichMapFunction<Row, DevicePojo> {
         @Override
         public DevicePojo map(Row row) {
-            return new DevicePojo(tupleType, row.getField(0), row.getField(1), row.getField(2), row.getField(3), row.getField(4), Arrays.asList(row.getField(5)));
+            return new DevicePojo(tupleType, row.getField(0), row.getField(1), row.getField(2), (String[]) row.getField(3),
+                    (String[]) row.getField(4), (Row[]) row.getField(5));
         }
     }
 
     public static class DeviceFilter implements FilterFunction<DevicePojo> {
         @Override
         public boolean filter(DevicePojo devicePojo) {
-
             return devicePojo.getFrequency() != null && devicePojo.getFrequency().size() >= 2;
         }
     }
